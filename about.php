dcb99fa0e9a42004ccdc8f2463360723db44a5d2
RizalNR <rizal.naufal@mhs.itenas.ac.id>
2023-05-23 16:18:45 +0700

    commit update about

--- about.php
+++ about.php
@@ -28,7 +28,7 @@ if (isset($_SESSION["driver"])) {
     <meta name="description" content="" />
     <meta name="author" content="" />
 
-    <title>Toko Supplier Daging Ayam Segar</title>
+    <title>Toko Supplier Daging Ayam Zegar</title>
 
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
     <link href="assets/style/main.css" rel="stylesheet" />
